754a781154152e2d574a87a71b9536e61d60930a
Jimmy CHEN-MA <jichen-m@e3r8p21.42.fr>
2018-06-18 12:38:55 +0200

    gitignore test

---
+++ Assets/_Scripts/LightWin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightWin : MonoBehaviour {
+
+	public GameObject			blurObj;
+	public void WinEvent()
+	{
+		Time.timeScale = 1f;
+		blurObj.SetActive(true);
+	}
+}
