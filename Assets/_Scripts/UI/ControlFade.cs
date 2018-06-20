b731a61431f6fa7f787641c02f3bb70195c45ad4
Jimmy CHEN-MA <jichen-m@e3r8p21.42.fr>
2018-06-20 19:42:44 +0200

    add Controls menu and fade animation

---
+++ Assets/_Scripts/UI/ControlFade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlFade : MonoBehaviour
+{
+	public int		secondsToFade;
+	public Animator	PanelAnimator;
+	public Animator	TextAnimator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(waitToFade(secondsToFade));
+	}
+	
+	IEnumerator waitToFade(int secondsToFade)
+	{
+		yield return new WaitForSeconds(secondsToFade);
+		PanelAnimator.SetTrigger("ControlFade");
+		TextAnimator.SetTrigger("ControlFade");
+	}
+
+}
